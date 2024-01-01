d3d633c0fc959dc5a895cbb0b6f19a8d1c6f9d01
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2024-01-01 10:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function  togglePanel(panelId) {
+	panels.forEach(panel =>{
+		panel.classlist.remove('active');
+	});
+
+	const clickPanel = document.getElementById(panelId);
+	clickPanel.classList.add("active");
+}
